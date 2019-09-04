2bb50b09294abbef10e2ab395a64d89bed06d5d9
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:19:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,8 @@ def save
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
 end
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

1ebb00bf5a8301917315e0d3d9a21c7d25b05151
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:20:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,11 @@ def save
 
 end
 
-
+def self.create(name:, grade:)
+  song = Song.new(name, grade)
+  song.save
+  song
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

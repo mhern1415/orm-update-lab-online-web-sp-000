8235df23fefa2317281a5c00da0e0419df86bab7
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:24:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,11 @@ def save
     student
   end
   
+   def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+  
     def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)

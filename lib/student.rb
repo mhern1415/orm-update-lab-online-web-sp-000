6cf8b7247f0c312fb7b46a7f4b19ecc2337f10a5
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:12:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,15 @@ end
 def self.drop_table 
   DB[:conn].execute("DROP TABLE IF EXISTS students")
 end
+
+def save
+  sql = <<-SQL
+      INSERT INTO songs (name, grade)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

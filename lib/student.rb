c3385a7289789ff9165deef4732f1a9170f5adab
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:10:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ def self.create_table
     DB[:conn].execute(sql)
 end
 
-
+def self.drop_table 
+  DB[:conn].execute("DROP TABLE IF EXISTS students")
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

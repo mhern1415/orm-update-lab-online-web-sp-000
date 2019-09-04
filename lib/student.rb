4ce7cebcb8450abf8d9726e251fb93a774768c02
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:09:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,15 @@ def initialize (id = nil, name, grade)
 end
 
 def self.create_table
-  
+   sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

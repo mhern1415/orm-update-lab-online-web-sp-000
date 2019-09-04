a461ee6eb65b4a6e3e922f26bc7f936b26f56353
Mark Hernandez <m.hern1415@gmail.com>
2019-09-04 01:24:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,12 @@ def save
     end
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end 
 
  def self.create(name:, grade:)
     student = Student.new(name, grade)
